dabbec0e15aa0cd9c02497fe11afaaf44f73c854
andreendo <andreendo@gmail.com>
2018-04-18 10:10:00 -0300

    first commit

---
+++ src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
@@ -0,0 +1,88 @@
+package br.edu.utfpr.exemplomaven;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author andreendo
+ */
+public class ExemploTest {
+
+    /**
+     * Vc precisa identificar onde estah o chromedriver. Baixar de:
+     * https://sites.google.com/a/chromium.org/chromedriver/downloads
+     *
+     * Versão utilizada do chromedriver: 2.35.528139
+     */
+    private static String CHROMEDRIVER_LOCATION = "/home/utfpr/install/selenium/chromedriver";
+    
+    private static int scId = 0;
+
+    WebDriver driver;
+    
+    @BeforeClass
+    public static void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_LOCATION);
+    }
+    
+    @Before
+    public void before() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //Opcao headless para MacOS e Linux
+        chromeOptions.addArguments("headless");
+        chromeOptions.addArguments("window-size=1200x600");
+        chromeOptions.addArguments("start-maximized");
+        
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    
+    @After
+    public void after() {
+        driver.close();
+    }
+    
+    @Test
+    public void testGoogleSearch() {
+        driver.get("https://www.google.com.br/");
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("teste de software");
+        
+        takeScreenShot();
+        
+        searchInput.submit();
+        
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.getTitle().toLowerCase().startsWith("teste") );
+        
+        takeScreenShot();
+        
+        assertTrue(driver.getTitle().startsWith("teste de software"));
+    }
+    
+    private void takeScreenShot() {
+        File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            scId++;
+            FileUtils.copyFile(sourceFile, new File("./res/" + scId + ".png"));
+        } catch(IOException e) {}
+    }
+}
